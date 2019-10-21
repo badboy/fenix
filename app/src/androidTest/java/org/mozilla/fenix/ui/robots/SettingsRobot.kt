5a40d1a6ef99daa4fd37aa2fc5415ca815a4a431
Richard Pappalardo <rpappalax@gmail.com>
2019-10-21 16:03:18 -0700

    Add UI test for sub menus in settings privacy

--- app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsRobot.kt
+++ app/src/androidTest/java/org/mozilla/fenix/ui/robots/SettingsRobot.kt
@@ -10,16 +10,19 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
-import androidx.test.espresso.matcher.ViewMatchers.Visibility
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Until
 import org.hamcrest.CoreMatchers
 import org.mozilla.fenix.helpers.TestAssetHelper.waitingTime
 import org.mozilla.fenix.helpers.TestHelper
+import org.mozilla.fenix.helpers.TestHelper.scrollToElementByText
 import org.mozilla.fenix.helpers.click
+import androidx.test.espresso.matcher.ViewMatchers.Visibility
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.CoreMatchers.endsWith
 
 /**
  * Implementation of Robot Pattern for the settings menu.
@@ -28,33 +31,52 @@ class SettingsRobot {
 
     // BASICS SECTION
     fun verifyBasicsHeading() = assertBasicsHeading()
+
     fun verifySearchEngineButton() = assertSearchEngineButton()
+
     fun verifyThemeButton() = assertThemeButton()
+
     fun verifyThemeSelected() = assertThemeSelected()
+
     fun verifyAccessibilityButton() = assertAccessibilityButton()
+
     fun verifySetAsDefaultBrowserButton() = assertSetAsDefaultBrowserButton()
 
     // PRIVACY SECTION
     fun verifyPrivacyHeading() = assertPrivacyHeading()
+
     fun verifyEnhancedTrackingProtectionButton() = assertEnhancedTrackingProtectionButton()
+
     fun verifyEnhancedTrackingProtectionValue() = assertEnhancedTrackingProtectionValue()
+
     fun verifyAddPrivateBrowsingShortcutButton() = assertAddPrivateBrowsingShortcutButton()
+
     fun verifySitePermissionsButton() = assertSitePermissionsButton()
+
     fun verifyDeleteBrowsingDataButton() = assertDeleteBrowsingDataButton()
+
     fun verifyDeleteBrowsingDataOnQuitButton() = assertDeleteBrowsingDataOnQuitButton()
+
     fun verifyDataCollectionButton() = assertDataCollectionButton()
+
     fun verifyPrivacyNoticeButton() = assertPrivacyNoticeButton()
+
     fun verifyLeakCanaryButton() = assertLeakCanaryButton()
+
     fun verifySettingsView() = assertSettingsView()
 
     // DEVELOPER TOOLS SECTION
     fun verifyDeveloperToolsHeading() = assertDeveloperToolsHeading()
+
     fun verifyRemoteDebug() = assertRemoteDebug()
 
     // ABOUT SECTION
     fun verifyAboutHeading() = assertAboutHeading()
+
     fun verifyHelp() = assertHelp()
+
     fun verifyRateOnGooglePlay() = assertRateOnGooglePlay()
+
     fun verifyAboutFirefoxPreview() = assertAboutFirefoxPreview()
 
     class Transition {
@@ -89,8 +111,8 @@ class SettingsRobot {
 
         fun openAccessibilitySubMenu(interact: SettingsSubMenuAccessibilityRobot.() -> Unit): SettingsSubMenuAccessibilityRobot.Transition {
             mDevice.waitForIdle()
-            fun accessibilityButton() = onView(ViewMatchers.withText("Accessibility"))
-            accessibilityButton().click()
+            fun transitionButton() = onView(ViewMatchers.withText("Accessibility"))
+            transitionButton().click()
 
             SettingsSubMenuAccessibilityRobot().interact()
             return SettingsSubMenuAccessibilityRobot.Transition()
@@ -98,12 +120,62 @@ class SettingsRobot {
 
         fun openDefaultBrowserSubMenu(interact: SettingsSubMenuDefaultBrowserRobot.() -> Unit): SettingsSubMenuDefaultBrowserRobot.Transition {
             mDevice.waitForIdle()
-            fun defaultBrowserButton() = onView(ViewMatchers.withText("Set as default browser"))
-            defaultBrowserButton().click()
+            fun transitionButton() = onView(ViewMatchers.withText("Set as default browser"))
+            transitionButton().click()
 
             SettingsSubMenuDefaultBrowserRobot().interact()
             return SettingsSubMenuDefaultBrowserRobot.Transition()
         }
+
+        fun openSubMenuEnhancedTP(interact: SettingsSubMenuEnhancedTPRobot.() -> Unit): SettingsSubMenuEnhancedTPRobot.Transition {
+            mDevice.waitForIdle()
+            fun transitionButton() = onView(ViewMatchers.withText("Enhanced Tracking Protection"))
+            transitionButton().click()
+
+            SettingsSubMenuEnhancedTPRobot().interact()
+            return SettingsSubMenuEnhancedTPRobot.Transition()
+        }
+
+        fun openSubMenuSitePermissions(interact: SettingsSubMenuSitePermissionsRobot.() -> Unit): SettingsSubMenuSitePermissionsRobot.Transition {
+            mDevice.waitForIdle()
+            scrollToElementByText("Delete browsing data")
+            fun transitionButton() = onView(ViewMatchers.withText("Site permissions"))
+            transitionButton().click()
+
+            SettingsSubMenuSitePermissionsRobot().interact()
+            return SettingsSubMenuSitePermissionsRobot.Transition()
+        }
+
+        fun openSubMenuDeleteBrowsingData(interact: SettingsSubMenuDeleteBrowsingDataRobot.() -> Unit): SettingsSubMenuDeleteBrowsingDataRobot.Transition {
+            mDevice.waitForIdle()
+            onView(withText(endsWith("Delete browsing data"))).click()
+
+            SettingsSubMenuDeleteBrowsingDataRobot().interact()
+            return SettingsSubMenuDeleteBrowsingDataRobot.Transition()
+        }
+
+        fun openSubMenuDeleteBrowsingDataOnQuit(interact: SettingsSubMenuDeleteBrowsingDataOnQuitRobot.() -> Unit): SettingsSubMenuDeleteBrowsingDataOnQuitRobot.Transition {
+            mDevice.waitForIdle()
+            onView(withText(endsWith("Delete browsing data on quit"))).click()
+
+            SettingsSubMenuDeleteBrowsingDataOnQuitRobot().interact()
+            return SettingsSubMenuDeleteBrowsingDataOnQuitRobot.Transition()
+        }
+
+        fun openDataCollection(interact: SettingsSubMenuDataCollectionRobot.() -> Unit): SettingsSubMenuDataCollectionRobot.Transition {
+            mDevice.waitForIdle()
+            onView(withText(endsWith("Data collection"))).click()
+
+            SettingsSubMenuDataCollectionRobot().interact()
+            return SettingsSubMenuDataCollectionRobot.Transition()
+        }
+    }
+    fun openSubMenuDeleteBrowsingDataOnQuit(interact: SettingsSubMenuDeleteBrowsingDataOnQuitRobot.() -> Unit): SettingsSubMenuDeleteBrowsingDataOnQuitRobot.Transition {
+        mDevice.waitForIdle()
+        onView(withText(endsWith("Delete browsing data on quit"))).click()
+
+        SettingsSubMenuDeleteBrowsingDataOnQuitRobot().interact()
+        return SettingsSubMenuDeleteBrowsingDataOnQuitRobot.Transition()
     }
 }
 
@@ -118,17 +190,22 @@ private fun assertSettingsView() {
 // BASICS SECTION
 private fun assertBasicsHeading() = onView(ViewMatchers.withText("Basics"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertSearchEngineButton() {
     mDevice.wait(Until.findObject(By.text("Search")), waitingTime)
     onView(ViewMatchers.withText("Search"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertThemeButton() = onView(ViewMatchers.withText("Theme"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertThemeSelected() = onView(ViewMatchers.withText("Light"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertAccessibilityButton() = onView(ViewMatchers.withText("Accessibility"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertSetAsDefaultBrowserButton() = onView(ViewMatchers.withText("Set as default browser"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
@@ -137,28 +214,41 @@ private fun assertPrivacyHeading() {
     onView(ViewMatchers.withText("Privacy"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertEnhancedTrackingProtectionButton() = onView(ViewMatchers.withText("Enhanced Tracking Protection"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertEnhancedTrackingProtectionValue() = onView(ViewMatchers.withText("On"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertAddPrivateBrowsingShortcutButton() {
     mDevice.wait(Until.findObject(By.text("Add private browsing shortcut")), waitingTime)
     onView(ViewMatchers.withText("Add private browsing shortcut"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertSitePermissionsButton() {
     TestHelper.scrollToElementByText("Site permissions")
     onView(ViewMatchers.withText("Site permissions"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
-private fun assertDeleteBrowsingDataButton() = onView(ViewMatchers.withText("Delete browsing data"))
-    .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-private fun assertDeleteBrowsingDataOnQuitButton() = onView(ViewMatchers.withText("Delete browsing data on quit"))
-    .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+private fun assertDeleteBrowsingDataButton() {
+    onView(withText(endsWith("Delete browsing data"))).click()
+        .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+}
+
+private fun assertDeleteBrowsingDataOnQuitButton() {
+    onView(withText(endsWith("Delete browsing data on quit"))).click()
+        .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+}
+
 private fun assertDataCollectionButton() = onView(ViewMatchers.withText("Data collection"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertPrivacyNoticeButton() = onView(ViewMatchers.withText("Privacy notice"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
 private fun assertLeakCanaryButton() = onView(ViewMatchers.withText("LeakCanary"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
@@ -168,6 +258,7 @@ private fun assertDeveloperToolsHeading() {
     onView(ViewMatchers.withText("Developer tools"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertRemoteDebug() = onView(ViewMatchers.withText("Remote debugging via USB"))
     .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
@@ -177,16 +268,19 @@ private fun assertAboutHeading() {
     onView(ViewMatchers.withText("About"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertHelp() {
     TestHelper.scrollToElementByText("About Firefox Preview")
     onView(ViewMatchers.withText("Help"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertRateOnGooglePlay() {
     TestHelper.scrollToElementByText("About Firefox Preview")
     onView(ViewMatchers.withText("Rate on Google Play"))
         .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 }
+
 private fun assertAboutFirefoxPreview() {
     TestHelper.scrollToElementByText("About Firefox Preview")
     onView(ViewMatchers.withText("About Firefox Preview"))
